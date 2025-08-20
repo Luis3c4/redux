39b2476c02f34e2d347f62d682b94c1789de78ea
luis4c4 <luisenriquechoqque@gmail.com>
2025-08-20 12:07:57 -0500

    redux-thunk

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,13 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import cakeReducer from '../features/cake/cakeSlice.js';
 import icecreamReducer from '../features/icecream/iceCream.js';
+import userReducer from '../features/user/userSlice.js';
 import pkg from "redux-logger";
 const { createLogger } = pkg;
 const logger = createLogger();
 export const store = configureStore({
     reducer: {
         cake:cakeReducer,
-        ice:icecreamReducer
+        ice:icecreamReducer,
+        user: userReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,17 +1,20 @@
 import { store } from "./app/store.js"; 
 import { ordered, restocked } from "./features/cake/cakeSlice.js";
 import { icecreamOrdered, icecreamRestocked } from "./features/icecream/iceCream.js";
+import { fetchUsers } from "./features/user/userSlice.js";
+
 console.log("initial state", store.getState())
 store.subscribe(()=>{
 })
-store.dispatch(ordered())
-store.dispatch(ordered())
-store.dispatch(ordered())
-store.dispatch(ordered())
-store.dispatch(restocked(5))
+store.dispatch(fetchUsers())
+// store.dispatch(ordered())
+// store.dispatch(ordered())
+// store.dispatch(ordered())
+// store.dispatch(ordered())
+// store.dispatch(restocked(5))
 
-store.dispatch(icecreamOrdered())
-store.dispatch(icecreamOrdered())
-store.dispatch(icecreamOrdered())
-store.dispatch(icecreamOrdered())
-store.dispatch(icecreamRestocked(5))
+// store.dispatch(icecreamOrdered())
+// store.dispatch(icecreamOrdered())
+// store.dispatch(icecreamOrdered())
+// store.dispatch(icecreamOrdered())
+// store.dispatch(icecreamRestocked(5))
