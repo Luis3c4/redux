fb86e749ddfbd0903878c8402438af90a319f699
luis3c4 <u20231587@utp.edu.pe>
2025-08-22 18:06:29 -0500

    dispatch

--- react-rkt-demo/src/features/icecream/inceView.jsx
+++ react-rkt-demo/src/features/icecream/inceView.jsx
@@ -1,15 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
-
+import { icecreamOrdered, icecreamRestocked } from "./iceCream";
+import { useState } from "react";
 function IceceamView() {
-    const numOfIcecreams = useSelector((state)=>state.ice.numOfIcecreams)
-    const dispatch = useDispatch();
-    return ( 
-        <div>
-            <h2>Number of icecream - {numOfIcecreams} </h2>
-            <button >Order icecream</button>
-            <button >Restore icecream</button>
-        </div>
-     );
+  const numOfIcecreams = useSelector((state) => state.ice.numOfIcecreams);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <h2>Number of icecream - {numOfIcecreams} </h2>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+
+      <button onClick={() => dispatch(icecreamOrdered())}>
+        Order icecream
+      </button>
+      <button onClick={() => dispatch(icecreamRestocked(value))}>
+        Restore icecream
+      </button>
+    </div>
+  );
 }
 
 export default IceceamView;
--- react-rkt-demo/src/features/user/useView.jsx
+++ react-rkt-demo/src/features/user/useView.jsx
@@ -1,9 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
+import { useEffect } from "react";
 function UserView() {
-    return ( 
-        <div>
-            <h2>list of users</h2>
-        </div>
-     );
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+  console.log(user);
+  return (
+    <div>
+      <h2>list of users</h2>
+      {user.loading && <h2>Loading...</h2>}
+      {user.error && <h2>{user.error}</h2>}
+      <ul>
+        {user.users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default UserView;
